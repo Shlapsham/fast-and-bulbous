ccbbedb0022e1bd7fc6c3733a9737fef7ac9ea46
Shlapsham <shlapsham@gmail.com>
2019-10-22 15:11:41 +1100

    Starting Point
    Basic movement, need to test spellcasting stuff

---
+++ SDDMajorWork/Assets/basicMovement.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicMovement : MonoBehaviour
+{
+    Transform myTransform;
+    [SerializeField]
+    float maxSpeed = 20f;
+    float momentumX = 0f;
+    float momentumY = 0f;
+    [SerializeField]
+    float acceleration = 0.5f;
+    void Start()
+    {
+        myTransform = transform;
+    }
+    void Update()
+    {
+        Movement();
+        CollisionCheck();
+        myTransform.position = (new Vector2(myTransform.position.x + LogisticFunction(maxSpeed, acceleration, momentumX), myTransform.position.y + LogisticFunction(maxSpeed, acceleration, momentumY)));
+    }
+    void Movement()
+    {
+        if (UnityEngine.Input.GetAxis("LeftStickVertical") < 0)
+        {
+            if (momentumY < 10 / acceleration)
+            {
+                momentumY = momentumY - UnityEngine.Input.GetAxis("LeftStickVertical");
+            }
+        }
+        else if (momentumY > 0)
+        {
+            momentumY--;
+            momentumY = Mathf.Round(momentumY);
+        }
+        if (UnityEngine.Input.GetAxis("LeftStickVertical") > 0)
+        {
+            if (momentumY > -10 / acceleration)
+            {
+                momentumY = momentumY - UnityEngine.Input.GetAxis("LeftStickVertical");
+            }
+        }
+        else if (momentumY < 0)
+        {
+            momentumY++;
+            momentumY = Mathf.Round(momentumY);
+        }
+        if (UnityEngine.Input.GetAxis("LeftStickHorizontal") > 0)
+        {
+            if (momentumX < 10 / acceleration)
+            {
+                momentumX = momentumX + UnityEngine.Input.GetAxis("LeftStickHorizontal");
+            }
+        }
+        else if (momentumX > 0)
+        {
+            momentumX--;
+            momentumX = Mathf.Round(momentumX);
+        }
+        if (UnityEngine.Input.GetAxis("LeftStickHorizontal") < 0)
+        {
+            if (momentumX > -10 / acceleration)
+            {
+                momentumX = momentumX + UnityEngine.Input.GetAxis("LeftStickHorizontal");
+            }
+        }
+        else if (momentumX < 0)
+        {
+            momentumX++;
+            momentumX = Mathf.Round(momentumX);
+        }
+    }
+    void CollisionCheck()
+    {
+        RaycastHit2D upTR = Physics2D.Raycast(new Vector2(myTransform.position.x + 24f, myTransform.position.y + 24f), Vector2.up, 1f);
+        RaycastHit2D rightTR = Physics2D.Raycast(new Vector2(myTransform.position.x + 24f, myTransform.position.y + 24f), Vector2.right, 1f);
+        RaycastHit2D rightBR = Physics2D.Raycast(new Vector2(myTransform.position.x + 24f, myTransform.position.y - 24f), Vector2.right, 1f);
+        RaycastHit2D downBR = Physics2D.Raycast(new Vector2(myTransform.position.x + 24f, myTransform.position.y - 24f), Vector2.down, 1f);
+        RaycastHit2D downBL = Physics2D.Raycast(new Vector2(myTransform.position.x - 24f, myTransform.position.y - 24f), Vector2.down, 1f);
+        RaycastHit2D leftBL = Physics2D.Raycast(new Vector2(myTransform.position.x - 24f, myTransform.position.y - 24f), Vector2.left, 1f);
+        RaycastHit2D leftTL = Physics2D.Raycast(new Vector2(myTransform.position.x - 24f, myTransform.position.y + 24f), Vector2.left, 1f);
+        RaycastHit2D upTL = Physics2D.Raycast(new Vector2(myTransform.position.x - 24f, myTransform.position.y + 24f), Vector2.up, 1f);
+        if (upTR && upTL)
+        {
+            if (momentumY > 0)
+            {
+                momentumY = -momentumY / 2f;
+            }
+        }
+        if (rightTR && rightBR)
+        {
+            if (momentumX > 0)
+            {
+                momentumX = -momentumX / 2f;
+            }
+        }
+        if (downBR && downBL)
+        {
+            if (momentumY < 0)
+            {
+                momentumY = -momentumY / 2f;
+            }
+        }
+        if (leftTL && leftBL)
+        {
+            if (momentumX < 0)
+            {
+                momentumX = -momentumX / 2f;
+            }
+        }
+    }
+        float LogisticFunction(float max, float growth, float x, float translateUp = 0)
+    {
+        float answer;
+        if (x > 0)
+        {
+            answer = max / (1 + Mathf.Pow((float)System.Math.E, -growth * (x - (5f / growth)))) + translateUp;
+        }
+        else if (x < 0)
+        {
+            answer = -max / (1 + Mathf.Pow((float)System.Math.E, -growth * (-x - (5f / growth)))) + translateUp;
+        }
+        else
+        {
+            answer = 0;
+        }
+        return answer;
+    }
+}
---
+++ SDDMajorWork/Assets/spellcasting.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    bool[] selectedKeys = { false, false, false, false, false, false, false, false, false, false, false };
+    bool[] testSpell = { true, true, true, true, true, true, true, true, true, true, true };
+    bool flag1 = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        checkKeyPress();
+        casting();
+    }
+    void checkKeyPress()
+    {
+        if (UnityEngine.Input.GetKey("A"))
+        {
+            if (selectedKeys[0])
+            {
+                selectedKeys[0] = false;
+            }
+            else
+            {
+                selectedKeys[0] = true;
+            }
+        }
+        if (UnityEngine.Input.GetKey("B"))
+        {
+            if (selectedKeys[1])
+            {
+                selectedKeys[1] = false;
+            }
+            else
+            {
+                selectedKeys[1] = true;
+            }
+        }
+        if (UnityEngine.Input.GetKey("X"))
+        {
+            if (selectedKeys[2])
+            {
+                selectedKeys[2] = false;
+            }
+            else
+            {
+                selectedKeys[2] = true;
+            }
+        }
+        if (UnityEngine.Input.GetKey("Y"))
+        {
+            if (selectedKeys[3])
+            {
+                selectedKeys[3] = false;
+            }
+            else
+            {
+                selectedKeys[3] = true;
+            }
+        }
+        if (UnityEngine.Input.GetKey("LeftBumper"))
+        {
+            if (selectedKeys[4])
+            {
+                selectedKeys[4] = false;
+            }
+            else
+            {
+                selectedKeys[4] = true;
+            }
+        }
+        if (UnityEngine.Input.GetKey("RightBumper"))
+        {
+            if (selectedKeys[5])
+            {
+                selectedKeys[5] = false;
+            }
+            else
+            {
+                selectedKeys[5] = true;
+            }
+        }
+        if (UnityEngine.Input.GetAxis("LeftTrigger") > 0)
+        {
+            if (selectedKeys[6])
+            {
+                selectedKeys[6] = false;
+            }
+            else
+            {
+                selectedKeys[6] = true;
+            }
+        }
+        if (UnityEngine.Input.GetAxis("DPADHorizontal") == 1)
+        {
+            if (selectedKeys[7])
+            {
+                selectedKeys[7] = false;
+            }
+            else
+            {
+                selectedKeys[7] = true;
+            }
+        }
+        if (UnityEngine.Input.GetAxis("DPADHorizontal") == -1)
+        {
+            if (selectedKeys[8])
+            {
+                selectedKeys[8] = false;
+            }
+            else
+            {
+                selectedKeys[8] = true;
+            }
+        }
+        if (UnityEngine.Input.GetAxis("DPADVertical") == 1)
+        {
+            if (selectedKeys[9])
+            {
+                selectedKeys[9] = false;
+            }
+            else
+            {
+                selectedKeys[9] = true;
+            }
+        }
+        if (UnityEngine.Input.GetAxis("DPADVertical") == -1)
+        {
+            if (selectedKeys[10])
+            {
+                selectedKeys[10] = false;
+            }
+            else
+            {
+                selectedKeys[10] = true;
+            }
+        }
+    }
+    void casting()
+    {
+        if (UnityEngine.Input.GetAxis("RightTrigger") > 0)
+        {
+            if (selectedKeys == testSpell)
+            {
+                flag1 = true;
+            }
+            for (int i = 0; i >= selectedKeys.Length; i++)
+            {
+                selectedKeys[i] = false;
+            }
+        }
+    }
+}
